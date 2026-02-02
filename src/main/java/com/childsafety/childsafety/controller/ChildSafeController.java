211377f71984cc2ca27174aa945e28ac01e301d6
Dinesh <ceoa491@gmail.com>
2026-02-02 18:09:23 +0530

    Use dynamic PORT for Render

--- src/main/java/com/childsafety/childsafety/controller/ChildSafeController.java
+++ src/main/java/com/childsafety/childsafety/controller/ChildSafeController.java
@@ -5,12 +5,14 @@ import com.childsafety.childsafety.service.ChildSafeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(
+        origins = "https://ceoa491-dev.github.io",
+        allowedHeaders = "*",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}
+)
 @RequestMapping("/child")
 public class ChildSafeController {
     @Autowired
