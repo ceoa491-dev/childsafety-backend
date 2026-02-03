0a6e792dfbffcf15d27bfb0e696b2b5234262bf7
Dinesh <ceoa491@gmail.com>
2026-02-03 23:01:11 +0530

    Your message

--- src/main/java/com/childsafety/childsafety/models/ChildSafeModel.java
+++ src/main/java/com/childsafety/childsafety/models/ChildSafeModel.java
@@ -3,10 +3,12 @@ package com.childsafety.childsafety.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "childsafety2")
 public class ChildSafeModel {
     @Id
     @GeneratedValue
